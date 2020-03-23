cb56f5c11a07283d9f92c434ba8ac44e15f0447c
Yuliia Nechyporuk <Yuliia_Nechyporuk@epam.com>
2020-03-23 19:54:55 +0200

    init commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Puppy();
+        }
+
+
+        public class Animal
+        {
+            public Animal()
+            {
+                Console.WriteLine("Hi from Animal");
+            }
+        }
+
+        public class Dog : Animal
+        {
+            public Dog() {
+                Console.WriteLine("Hi from Dog");
+            }
+        }
+
+        public class Puppy : Dog
+        {
+            public Puppy()
+            {
+                Console.WriteLine("Hi from Puppy!!");
+            }
+        }
+    }
+}
